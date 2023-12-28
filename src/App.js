73868ade7a8be1d6c7eb3985b7730976da0bba69
George <georgemitusov@gmail.com>
2023-12-28 16:44:33 +0100

    main page RD done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
-import React, { useContext, useEffect } from 'react';
-import { Context } from './context/Context';
+import React, { useContext, useEffect } from "react";
+import { Context } from "./context/Context";
 
-import About from './components/About';
-import Main from './components/Main';
-import Job from './components/Job';
-import Projects from './components/Projects';
-import Clients from './components/Clients';
-import Contact from './components/Contact';
-import ScrollToTopBtn from './components/ScrollToTopBtn';
-import Footer from './components/Footer';
-import Loader from './components/Loader';
+import About from "./components/About";
+import Main from "./components/Main";
+import Job from "./components/Job";
+import Projects from "./components/Projects";
+import Clients from "./components/Clients";
+import Contact from "./components/Contact";
+import ScrollToTopBtn from "./components/ScrollToTopBtn";
+import Footer from "./components/Footer";
+import Loader from "./components/Loader";
 
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence } from "framer-motion";
 
-import './styles/App.scss';
+import "./styles/App.scss";
 
 function App() {
-
   const { loading, setLoading, switchMenuOff } = useContext(Context);
 
   useEffect(() => {
-
     setLoading(true);
 
     setTimeout(() => {
-
       setLoading(false);
-
     }, 2200);
-
   }, []);
 
   return (
-    <div 
-      className="App"
-      onClick={switchMenuOff}
-    >
-
+    <div className="App" onClick={switchMenuOff}>
       <AnimatePresence mode="wait">
-
-        { loading ? (
+        {/* { loading ? (
           <Loader/>
         ) : (
           <div>
@@ -52,10 +42,18 @@ function App() {
             <Contact />
             <Footer />
           </div>
-        ) }
-      
+        ) } */}
+        <div>
+          <Main />
+          <ScrollToTopBtn />
+          <About />
+          <Job />
+          <Projects />
+          <Clients />
+          <Contact />
+          <Footer />
+        </div>
       </AnimatePresence>
-      
     </div>
   );
 }
