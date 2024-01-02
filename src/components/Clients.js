5f58742c770d21ef46b3f502d2312b1890b94409
George <georgemitusov@gmail.com>
2024-01-02 12:12:39 +0100

    clients RD done

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,74 +1,73 @@
-import React, { useContext } from 'react'
-import { Context } from '../context/Context';
-import { motion } from 'framer-motion';
+import React, { useContext } from "react";
+import { Context } from "../context/Context";
+import { motion } from "framer-motion";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-import SlickSlider from './SlickSlider';
+import SlickSlider from "./SlickSlider";
 
-import '../styles/Clients.scss';
+import "../styles/Clients.scss";
 
 const Clients = () => {
-
   const { sliderImages, usersData } = useContext(Context);
 
   const SlickArrowLeft = ({ currentSlide, slideCount, props, onClick }) => (
     <button
-      { ...props }
+      {...props}
       className="slick-arrow-left"
       aria-disabled={currentSlide === 0 ? true : false}
       onClick={onClick}
     >
-      <FontAwesomeIcon className='slick-arrow-left-icon' icon={faArrowLeft} />
+      <FontAwesomeIcon className="slick-arrow-left-icon" icon={faArrowLeft} />
     </button>
-  )
+  );
 
   const SlickArrowRight = ({ currentSlide, slideCount, props, onClick }) => (
     <button
-      { ...props }
+      {...props}
       className="slick-arrow-right"
       aria-disabled={currentSlide === slideCount - 1 ? true : false}
       onClick={onClick}
     >
-      <FontAwesomeIcon className='slick-arrow-right-icon' icon={faArrowRight} />
+      <FontAwesomeIcon className="slick-arrow-right-icon" icon={faArrowRight} />
     </button>
-  )
+  );
 
   const topSliderSettings = {
-      dots: true,
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      cssEase: 'linear',
-      responsive: [
-        {
-          breakpoint: 1050,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 5,
-          }
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
         },
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
-          }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
         },
-        {
-          breakpoint: 550,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
-          }
-        }
-      ]
-  }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+    ],
+  };
 
   const lowerSliderSettings = {
     dots: false,
@@ -80,51 +79,46 @@ const Clients = () => {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    cssEase: 'linear' 
-  }
+    cssEase: "linear",
+  };
 
   return (
-    <section className='clients' id='clients'>
-      
-      <div className='clients-container'>
-
-        <div className='clients-container-top-content'>
-
-          <motion.div className='clients-container-top-content-header'>
+    <section className="clients" id="clients">
+      <div className="clients-container">
+        <div className="clients-container-top-content">
+          <motion.div className="clients-container-top-content-header">
             <h3> OUR CLIENTS </h3>
-            <h1> Glint has been honored to partner <br /> up with these clients </h1>
+            <h1>
+              {" "}
+              Glint has been honored to partner <br /> up with these clients{" "}
+            </h1>
           </motion.div>
 
           <SlickSlider
             data={sliderImages}
             property={topSliderSettings}
-            structure='icon'
-            className='top-slick-slider'
-            className2='top-slider'
+            structure="icon"
+            className="top-slick-slider"
+            className2="top-slider"
           />
 
-          <div className='clients-container-top-content-bottom-line'></div>
-
+          <div className="clients-container-top-content-bottom-line"></div>
         </div>
-        
-        <div className='clients-container-bottom-content'>
 
+        <div className="clients-container-bottom-content">
           <SlickSlider
             data={usersData}
             property={lowerSliderSettings}
-            structure='lower'
-            className='lower-slick-slider'
-            className2='lower-slider'
+            structure="lower"
+            className="lower-slick-slider"
+            className2="lower-slider"
           />
-
         </div>
-
       </div>
 
-      <div className='clients-content-line'></div>
-
+      <div className="clients-content-line"></div>
     </section>
-  )
-}
+  );
+};
 
-export default Clients
+export default Clients;
